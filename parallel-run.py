408d2b04ec4d38280333229df205b958d5afcc59
Rohan Garg <rohgarg@ccs.neu.edu>
2017-06-02 18:19:20 -0400

    parallel-run: dynamic parallelism

--- parallel-run.py
+++ parallel-run.py
@@ -4,41 +4,58 @@ import subprocess, shlex
 import numpy as np
 from tqdm import tqdm
 from multiprocessing import Pool
+import sys
 
 results = {'computeTime': [],
            'aware-rt': [],
            'unaware-rt': [],
            'aware-ckpt': [],
            'unaware-ckpt': [],
+           'aware-lcw': [],
+           'unaware-lcw': [],
+           'aware-lrw': [],
+           'unaware-lrw': [],
            'aware-lw': [],
            'unaware-lw': []}
-RANGE=100
+RANGE=512
 
 def f(cmd):
     return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
 aux = open("aux-results.txt", "w")
 
+par = int(sys.argv[1])
+par = 2 if par < 2 else par
+max_par_runs = par/2
+
 with tqdm(total=RANGE*2) as pbar:
-    for i in range(RANGE):
+    for i in range(RANGE/(max_par_runs)):
         computeTime = 500
-        results['computeTime'].append(computeTime)
         cmdSort = "./rr-proposed-ckpt-sim.py -w -n 2 --run-time %d --sort" % (computeTime)
         cmdNoSort = "./rr-proposed-ckpt-sim.py -w -n 2 --run-time %d" % (computeTime)
-        arr = [cmdSort, cmdNoSort]
-        pool = Pool(processes=2)
+        a = [cmdSort, cmdNoSort]
+        arr = []
+        for j in range(max_par_runs):
+            arr.extend(a)
+        pool = Pool(processes=par)
         res = pool.map(f, arr)
         pool.close()
         pool.join()
-        aux.write("Aware:\n %s" % (res[0]))
-        aux.write("Unaware:\n %s" % (res[1]))
-        results['aware-rt'].append(int(res[0].split('\n')[-4].split(':')[1]))
-        results['unaware-rt'].append(int(res[1].split('\n')[-4].split(':')[1]))
-        results['aware-ckpt'].append(int(res[0].split('\n')[-3].split(':')[1]))
-        results['unaware-ckpt'].append(int(res[1].split('\n')[-3].split(':')[1]))
-        results['aware-lw'].append(int(res[0].split('\n')[-2].split(':')[1]))
-        results['unaware-lw'].append(int(res[1].split('\n')[-2].split(':')[1]))
-        pbar.update(2)
+        for j in range(max_par_runs):
+           aux.write("Aware:\n %s" % (res[j*2+0]))
+           aux.write("Unaware:\n %s" % (res[j*2+1]))
+           results['computeTime'].append(computeTime)
+           results['aware-rt'].append(int(res[j*2+0].split('\n')[-4].split(':')[1]))
+           results['unaware-rt'].append(int(res[j*2+1].split('\n')[-4].split(':')[1]))
+           results['aware-ckpt'].append(int(res[j*2+0].split('\n')[-3].split(':')[1]))
+           results['unaware-ckpt'].append(int(res[j*2+1].split('\n')[-3].split(':')[1]))
+           results['aware-lw'].append(int(res[j*2+0].split('\n')[-2].split(':')[1]))
+           results['unaware-lw'].append(int(res[j*2+1].split('\n')[-2].split(':')[1]))
+           results['aware-lcw'].append(int(res[j*2+0].split('\n')[-6].split(':')[1]))
+           results['unaware-lcw'].append(int(res[j*2+1].split('\n')[-6].split(':')[1]))
+           results['aware-lrw'].append(int(res[j*2+0].split('\n')[-5].split(':')[1]))
+           results['unaware-lrw'].append(int(res[j*2+1].split('\n')[-5].split(':')[1]))
+        pbar.update(par)
 
 aux.close()
 
@@ -47,6 +64,10 @@ x = np.column_stack((results['computeTime'],
                     results['unaware-rt'],
                     results['aware-ckpt'],
                     results['unaware-ckpt'],
+                    results['aware-lcw'],
+                    results['unaware-lcw'],
+                    results['aware-lrw'],
+                    results['unaware-lrw'],
                     results['aware-lw'],
                     results['unaware-lw']))
 head = 'computeTime,'\
@@ -54,6 +75,10 @@ head = 'computeTime,'\
        'unaware-rt,'\
        'aware-ckpt,'\
        'unaware-ckpt,'\
+       'aware-lcw,'\
+       'unaware-lcw,'\
+       'aware-lrw,'\
+       'unaware-lrw,'\
        'aware-lw,'\
        'unaware-lw'
 np.savetxt("results.csv", x.astype(int), delimiter=',', header=head)
