578512c760b8f9e08d168917a0b2ef5f96f09f94
Tirthak <patel.ti@husky.neu.edu>
2017-09-30 10:50:11 -0400

    Adding dmtcp_opts for compare methods and run scripts.

--- scheduler/run_compare_methods.sh
+++ scheduler/run_compare_methods.sh
@@ -1,1 +1,2 @@
-python compare_methods.py -d ../../dmtcp -l ../../dmtcp/test/dmtcp2 -g ../../dmtcp/test/dmtcp1 -t 1000 -m 20 -c 23 -i 1.0541 -n 3.3333 -w 0.6 -s 200
+python compare_methods.py -d ../../dmtcp -l "mpirun -np 1 ./bin/CoMD-mpi1 -e -i 1 -j 1 -k 1 -x 20 -y 20 -z 20" -g "mpirun -np 1 ./bin/CoMD-mpi2 -e -i 1 -j 1 -k 1 -x 75 -y 75 -z 75" -t 100 -m 2 -c 5 -i 0.1563 -n 0.4944 -w 0.6 -s 20 -o " --no-gzip"
+
